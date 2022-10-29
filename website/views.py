a96dbfa9b97dea7c2219d48ccd248c505b396f94
v0es <markkikov@gmail.com>
2022-10-30 00:03:59 +0300

    web page draft

--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from website import app
 
-from flask import render_template
+
+from flask import render_template, request
 
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'post':
+        pass
+
+
+
     return render_template('index.html')
